5f9825a739f8f56ec23bfa394eb6fea053826f20
muhwezi deo <aggrey256@gmail.com>
2019-07-29 15:40:19 +0300

    chore(setup): setup express app with typescript [Delivers #167566632]

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {Response, Request} from 'express';
+
+import  app from './app';
+
+const port = process.env.port || 3000;
+
+app.use('/', (req: Request, res: Response) => {
+
+    return res.json({
+        message: 'Welcome To BDIRS'
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Running on ${port}`);
+});
