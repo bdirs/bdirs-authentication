fd0e88f688c2a468097b596ffaca04e24514cfd1
fahad <fahdjamy2@gmail.com>
2019-07-30 11:57:46 +0300

    ch-heroku(Setup-heroku): Create heroku pipelines
    - setup heroku
    - create pipelines for the app.
    
    [Delivers #167511346]

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import {Response, Request} from 'express';
 
 import  app from './app';
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', (req: Request, res: Response) => {
-
-    return res.json({
-        message: 'Welcome To BDIRS'
-    })
+  return res.status(200).json({
+    message: 'Welcome To BDIRS',
+    status: 'success',
+  })
 });
 
 app.listen(port, () => {
-    console.log(`Running on ${port}`);
+  console.log(`Running on ${port}`);
 });
