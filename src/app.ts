cbc69104d49bd368572404965566589f62024e03
muhwezi deo <aggrey256@gmail.com>
2019-07-31 12:34:22 +0300

    chore(test): setup jest test enviroment - add jest dependencies - ensure tests can run on cli

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/", (req: Request, res: Response) => {
+
+    return res.json({
+        message: "Welcome To BDIRS",
+    });
+});
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import { Response, Request } from 'express';
-
-import  app from './app';
+import app from "./app";
 
 const port = process.env.PORT || 3000;
-
-app.use('/', (req: Request, res: Response) => {
-
-    return res.json({
-        message: 'Welcome To BDIRS'
-    })
-});
 
 app.listen(port, () => {
     console.log(`Running on ${port}`);
