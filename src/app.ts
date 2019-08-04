0ab8bab67e6dc869d537c0f88393b934421f20b6
muhwezi deo <aggrey256@gmail.com>
2019-08-04 12:48:09 +0300

    chore(sentry): setup sentry for error reporting
    [Delivers #167561219]

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
 app.use(express.json());
+
+Sentry.init({
+    dsn: "https://5ed302e0f47d4935ad60ce898f3ff79a@sentry.io/1521028"
+});
 
 app.use("/", (req: Request, res: Response) => {
 
