c8b295f53a06d374a1080f6c5043c547f6fb8057
muhwezideo <aggrey256@gmail.com>
2019-12-28 12:36:10 +0300

    add cors and env example file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 // tslint:disable-next-line:no-var-requires
+import cors from "cors";
 import express, { Request, Response } from "express";
 import rolesRouter from "./api/roles";
 import userRouters from "./api/users";
 
 const app = express();
+const apiPrefix = "/api/v1";
 
 app.use(express.json());
+app.use(cors());
 
-const apiPrefix = "/api/v1";
 app.use(`${apiPrefix}/users`, userRouters);
 app.use(`${apiPrefix}/roles`, rolesRouter);
 
