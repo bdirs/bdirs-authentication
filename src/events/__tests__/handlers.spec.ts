93652ba79940f4754c2a9d1f7c44b7df240aaf72
muhwezideo <aggrey256@gmail.com>
2020-06-15 09:08:27 +0300

    fix failing tests on travis

--- src/events/__tests__/handlers.spec.ts
+++ src/events/__tests__/handlers.spec.ts
@@ -2,13 +2,17 @@ import * as helpers from "../../helpers";
 import {IAdminEmail} from "../../helpers/email-helper";
 import { newAdminRegisteredHandler } from "../handlers";
 
+jest.mock("../../helpers/email-helper.ts", () => ({
+    sendAdminConfirmationEmail: jest.fn(),
+}));
+
 describe("Handlers", () => {
     it("should handle new admin registration event", async () => {
         const data: IAdminEmail = {
-            email: "test@test",
-            username: "username",
             addedBy: "me",
+            email: "test@test",
             password: "password",
+            username: "username",
         };
         jest.spyOn(helpers, "sendAdminConfirmationEmail").mockResolvedValueOnce(null);
         await newAdminRegisteredHandler(data);
